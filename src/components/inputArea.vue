6ff187b3db00cb1df89d227db672267f4ecda27a
Aska <2535094736@qq.com>
2020-10-13 23:19:07 +0800

    module input

---
+++ src/components/inputArea.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-row :gutter="20">
+    <el-col
+      v-for="item in hcpInfo"
+      :key="item.znTitle"
+      class="item_margin"
+      :span="item.isnecessary == 'necessary' ? 24 : 12"
+    >
+      <el-row v-if="item.isnecessary" :gutter="20">
+        <el-col :span="4" class="item_label"
+          >{{item.znTitle}}<span v-if="isNecessarySignal" class="necessary_signal">*</span></el-col
+        >
+        <el-col :span="8"
+          ><el-input v-model="item.input" :placeholder="'请输入' + item.znTitle"></el-input
+        ></el-col>
+        <el-col class="alert_icon" :span="1">
+          <el-popover
+            placement="top-start"
+            width="200"
+            trigger="hover"
+            content="输入大交通占总预算的最高比例"
+          >
+            <i class="el-icon-warning" slot="reference"></i>
+          </el-popover>
+        </el-col>
+      </el-row>
+      <el-row v-else :gutter="20">
+        <el-col :span="8" class="item_label"
+          >{{ "HCP" + item.znTitle
+          }}<span v-if="isNecessarySignal" class="necessary_signal">*</span></el-col
+        >
+        <el-col v-if="item.type=='input'" :span="16"
+          ><el-input 
+            v-model="item.input"
+            :placeholder="'请输入' + item.znTitle"
+          ></el-input
+        ></el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  props: {
+    hcpInfo: {
+      type: Array,
+      default: [],
+    },
+    isNecessarySignal: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/newHcp.vue
+++ src/views/newHcp.vue
@@ -8,7 +8,7 @@
         </div>
       </div>
       <div class="mainer">
-        <el-row class="title">会议名称：广州肿瘤会议 会议编号：EPG</el-row>
+        <el-row class="title">会议名称：广州肿瘤会议 会议编号：EPG567</el-row>
         <el-card shadow="never">
           <div class="title_small">
             <span>HCP 基本信息</span
@@ -18,6 +18,7 @@
               ></span
             >
           </div>
+          <InputArea :hcpInfo='hcpInfo' :isNecessarySignal='true'></InputArea>
         </el-card>
       </div>
     </div>
@@ -27,15 +28,52 @@
 <script>
 import BreadCrumb from "../components/breadCurmb";
 import Slider from "../components/Slider";
+import InputArea from "../components/inputArea";
 export default {
   data() {
     return {
       siderItem: "新建HCP",
+      input: "",
+      hcpInfo: [
+        {
+          enTitle: "UCI CODE",
+          znTitle: "UCI CODE",
+          input: "",
+          isnecessary: 'necessary',
+          type: "input",
+        },
+        { enTitle: "name", znTitle: "姓名", input: "", type: "input" },
+        {
+          enTitle: "companyCity",
+          znTitle: "单位所在城市",
+          input: "",
+          type: "input",
+        },
+        { enTitle: "gender", znTitle: "性别", input: "", type: "input" },
+        { enTitle: "city", znTitle: "所在城市", input: "", type: "input" },
+        { enTitle: "mobile", znTitle: "手机号", input: "", type: "input" },
+        { enTitle: "hostipal", znTitle: "所在医院", input: "", type: "input" },
+        { enTitle: "idNum", znTitle: "证件号", input: "", type: "input" },
+        {
+          enTitle: "department",
+          znTitle: "所在科室",
+          input: "",
+          type: "input",
+        },
+        {
+          enTitle: "licenceType",
+          znTitle: "证件类型",
+          input: "",
+          type: "input",
+        },
+        { enTitle: "title", znTitle: "职务", input: "", type: "input" },
+      ],
     };
   },
   components: {
     BreadCrumb,
     Slider,
+    InputArea,
   },
 };
 </script>
